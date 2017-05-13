7f9fb46fc58e983f8232f809c62aed3b9383537a
Marc Maguire <maguire.marc@gmail.com>
2017-05-13 19:34:47 -0400

    initial commit

---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Marc Maguire on 2017-05-13.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
